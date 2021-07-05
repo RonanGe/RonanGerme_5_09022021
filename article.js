247bf4c68133316e3395d62daed87eef6ec67c3e
Ronan Germe <ronangerme@yahoo.fr>
2021-07-05 10:52:31 +0200

    maj panier 2

--- article.js
+++ article.js
@@ -59,3 +59,52 @@ if(document.getElementById('btn') != null){
 }
   
 const mainCart = document.getElementById('mainCart')
+
+if(cartProducts.length > 0 && mainCart != null){
+
+  let form = document.getElementById('form')
+  form.style.display = 'flex'
+  let textFinalPrice = document.getElementById('finalPrice')
+  textFinalPrice.style.display = 'block'
+
+  for(let i = 0; i < cartProducts.length; i++){ 
+
+      // mise en page du panier
+
+      let sectionCartArticle = document.createElement('section')
+      mainCart.appendChild(sectionCartArticle)
+      sectionCartArticle.classList.add('cart_Article')
+      let imgCart = document.createElement("img")
+      sectionCartArticle.appendChild(imgCart)
+      imgCart.id = 'cart_photo'
+      imgCart.setAttribute('src', cartProducts[i].teddie.imageUrl)
+      let h1Cart = document.createElement("h1")
+      sectionCartArticle.appendChild(h1Cart)
+      h1Cart.innerHTML = cartProducts[i].name
+      let priceCart = document.createElement('p')
+      sectionCartArticle.appendChild(priceCart)
+      priceCart.id = 'cart_prix'
+      priceCart.innerHTML = parseInt(cartProducts[i].price / 100).toFixed(2) + '€'
+      let buttonCartTrash = document.createElement('button')
+      sectionCartArticle.appendChild(buttonCartTrash)
+      let iTrash = document.createElement('span')
+      buttonCartTrash.appendChild(iTrash)
+      iTrash.classList.add('fas', 'fa-trash')
+
+      // afficher le prix total à l'utilisateur
+      totalPrice += cartProducts[i].price
+      console.log(totalPrice)
+      let finalPrice = document.getElementById('totalPrice')
+      finalPrice.innerHTML = ' ' + parseInt(totalPrice / 100).toFixed(2) + ' €'
+      console.log(i + ' numéro ourson')
+      // supprimer un élément du panier
+      
+      buttonCartTrash.addEventListener('click', () => {
+          cartProducts.splice(i,1) //supprimer l'élément
+          console.log('element ' + cartProducts.name + 'supprimé !')
+          localStorage.clear() // nettoyer le localStorage
+          localStorage.setItem('LSCartProducts', JSON.stringify(cartProducts)) // réactualiser le panier
+          window.location.reload() // afficher la nouvelle fenêtre
+      })
+  }
+}

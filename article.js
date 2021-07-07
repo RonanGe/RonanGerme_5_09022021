b580f84d59e9ff2c8797ea4ad7b436990fec9cdf
Ronan Germe <ronangerme@yahoo.fr>
2021-07-07 15:23:04 +0200

    maj panier 6

--- article.js
+++ article.js
@@ -1,47 +1,77 @@
-function loadTeddy() {
-  let params = new URL(document.location).searchParams;
-  let id = params.get("id");
-  var ourson = new URL("http://localhost:3000/api/teddies/" + id);
-  fetch(ourson)
+// Ne pas utiliser "var" : Soit const ou let
+//Utilisation de const
+//cart : panier
+
+
+// J'initialise local storage au chargement de la page
+// window.onload : C'est une fonction qui se lance aussiôt la la fenêtre est chargée 
+window.onload=()=>{
+  localStorage.setItem("cart",JSON.stringify([]))
+}
+
+// ICI je défini l'URL de base
+const BASE_URL="http://localhost:3000/api/teddies/";
+
+// Je crée mes variable qui font référence à des élements du DOM
+const articlePicture= document.getElementById("image")
+const articleName= document.getElementById("name")
+const articleColors= document.getElementById("colors");
+const articlePrice= document.getElementById("prix");
+const articleDescription=document.getElementById("description");
+const articleImage=document.getElementById("image");
+
+
+// Ici je recupère l'ID
+const params = new URL(document.location).searchParams;
+const id = params.get("id");
+
+// Je crée l'objet qui va garder les informations sur l'articel chargé
+let selectedArticle={};
+
+// La fonction pour charger l'article par son ID
+function fetchArticleById(id) {
+  const urlToFetch=BASE_URL+id;
+  console.log(urlToFetch)
+  fetch(urlToFetch)
     .then((data) => data.json())
-    .then((teddie) => {
-      document.title = "Orinoco ~ " + teddie.name;
-      document.getElementById("nom").innerText = teddie.name;
-      document.getElementById("prix").innerText = teddie.price + "€";
-      document.getElementById("image").src = teddie.imageUrl;
-      document.getElementById("description").innerText = teddie.description;
-      teddie.colors.forEach(color=>{
-          document.getElementById("couleur").innerHTML +="<option value="+color+">"+color+"</option>";
-      }
-        
-        )
+    .then((article) => {
+
+      // article.color[0] : C'est définir comme couleur par défaut la première couleur de la liste des couleurs
+      selectedArticle={picture: article.name,name:article.name,color:article.colors[0],price:article.price};
+      updateDom(article)
     })
     .catch((err) => console.log(err));
 }
-loadTeddy();
+
+fetchArticleById(id);
+
+// Fonction pour mettre à jour les élements du DOM
+function updateDom(article){
+   document.title = "Orinoco ~ " + article.name;
+      articleName.innerText = article.name;
+      articlePrice.innerText = article.price + "€";
+      articleImage.src = article.imageUrl;
+      articleDescription.innerText = article.description;
+      article.colors.forEach(color=>{
+          articleColors.innerHTML +="<option value="+color+">"+color+"</option>";
+      })
+}
 
 
-const addCart = document.querySelector('button');
-addCart.addEventListener('click', function(e) { //evenement 'click' pour l'envoi au local storage
-  let teddiesChoosen = {
-        picture: document.getElementById("image"),
-        firstName: document.getElementById("nom"),
-        color: document.getElementById("couleur"),
-        price: document.getElementById("prix"),
-    }
-    const teddiesAdded = localStorage.getItem('product');
-    if(teddiesAdded) {
-      console.log("teddiesChoosen")
-        teddiesInCArt = JSON.parse(teddiesAdded);
-        teddiesInCArt.push(teddiesChoosen);
-        localStorage.setItem('product', JSON.stringify(teddiesInCArt));
-        alert('Ajouté au panier !');
-    } else {
-      console.log("teddiesChoosen")
-        teddiesInCArt = [];
-        teddiesInCArt.push(teddiesChoosen);
-        localStorage.setItem('product', JSON.stringify(teddiesInCArt));
-        alert('Ajouté au panier !');
-    }
+// Function pour mettre à jour l'attribut color de l'objet article quand l'utilisateur choisis une couleurs
+articleColors.addEventListener('change',(e)=>{
+  selectedArticle.color=e.target.value;
 })
 
+const addCart = document.querySelector('button');
+
+//evenement 'click' pour l'envoi au local storage
+addCart.addEventListener('click', function(e) { 
+
+  // Ici, plus besoin de vérifier se le localstorage est vide, parce qu'il a déjà initialisé au chargement de la page
+  //Avec window.onload
+  const cart = JSON.parse(localStorage.getItem('cart'));
+  cart.push(selectedArticle);
+  localStorage.setItem('cart', JSON.stringify(cart));
+  alert('Ajouté au panier !');
+})
--- panier.js
+++ panier.js
@@ -1,50 +1,33 @@
-let cartProducts = JSON.parse(localStorage.getItem("Panier"))
-console.log('LSCartProducts');
+let cartProducts = JSON.parse(localStorage.getItem("product"))
 
-let urlProduct = ''
+//Sélection de la classe où injecter le code
+const positionElement = document.querySelector("#mainCart"); 
+console.log(positionElement);
 
-  let totalPrice = 0
-  
-  let getProducts = function(){
-    return new Promise(function(resolve){
-        let request = new XMLHttpRequest()
-        request.onreadystatechange = function(){
-            if(request.readyState === XMLHttpRequest.DONE && request.status == 200){
-                resolve(JSON.parse(request.responseText))
-            }
-        }
-        request.open("GET", "http://localhost:3000/api/teddies" + '/' + urlProduct)
-        request.send()
-    })
-  }
+//si panier vide
 
-  const mainCart = document.getElementById('mainCart')
+if(cartProducts === null){
+    const panierVide = `
+    <div class="panier-vide">
+        <div> Le panier est vide</div>
+    </div>
+    `;
+    positionElement.innerHTML = panierVide;
+}
 
-  if(cartProducts.length > 0 && mainCart != null){
-  
-    let form = document.getElementById('form')
-    form.style.display = 'flex'
-    let textFinalPrice = document.getElementById('finalPrice')
-    textFinalPrice.style.display = 'block'
-  
-    for(let i = 0; i < cartProducts.length; i++){ 
-  
-        // mise en page du panier
-  
-        fetch("http://localhost:3000/api/teddies/")
-    .then((data) => data.json())
-    .then((teddie) => {
-      document.getElementById("nom").innerText = teddie.name;
-      document.getElementById("prix").innerText = teddie.price + "€";
-      document.getElementById("img").src = teddie.imageUrl;
-      document.getElementById("description").innerText = teddie.description;
-    })
-  
-        // afficher le prix total à l'utilisateur
-        totalPrice += cartProducts[i].price
-        let finalPrice = document.getElementById('totalPrice')
-        finalPrice.innerHTML = ' ' + parseInt(totalPrice / 100).toFixed(2) + ' €'
+else{
+    let structurePanier = [];
+
+    for(k = 0; k < cartProducts.lengh; k++){
+        structurePanier = structurePanier +=`
+        <div class="container">
+            ${cartProducts[k].picture}
+            <div>Quantité 1 - ${cartProducts[k].firstName} Prix : ${cartProducts[k].price}</div>
+        </div>
+        `;
     }
-  }
-  
+        if(k === cartProducts.lengh){
 
+        positionElement.innerHTML = structurePanier; 
+    }
+}
